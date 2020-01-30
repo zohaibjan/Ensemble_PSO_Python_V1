b51959ac02c5905261bc024cd8968fdede844982
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2020-01-30 10:54:54 +1000

    Update mainProgram.py

--- mainProgram.py
+++ mainProgram.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 24 11:17:12 2020
+
+@author: janz
+"""
+
 import warnings
 import loadData
 from trainClassifiers import trainClassifiers
@@ -6,13 +13,14 @@ import numpy as np
 from sklearn.model_selection import KFold
 from sklearn.cluster import KMeans
 from optimizeEnsemble import optimizeEnsemble
+import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
 warnings.filterwarnings('ignore')
 
 def mainProgram(dataset):
     # load data
     X, Y = loadData.load_dataset(dataset)
     fold = 10
-    num_of_clusters = 3
     kf = KFold(n_splits=fold)
     current_fold = 0
     acc = 0
@@ -23,23 +31,35 @@ def mainProgram(dataset):
     Y_train_clusters = []
     for train, test in kf.split(X):
     	X_train, X_test, Y_train, Y_test = X[train], X[test], Y[train], Y[test]
-    	# for k clusters
-    	for i in range(num_of_clusters):
-    		if i == 0:
-    			continue
-    		kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)			
-    		kmeans.fit(X_train)
-    		for j in range(i):
-    			X_train_temp = X_train[kmeans.labels_==j]
-    			Y_train_temp = Y_train[kmeans.labels_==j]
-    			v, c = np.unique(Y_train_temp, return_counts=True)	
-    			if v.shape[0] < 2 or 1 in c:
-    				print('Cluster ' + str(j) + ' only has one class: ' + str(v[0]))
-    			else:
-    				X_train_clusters.append(X_train_temp)
-    				Y_train_clusters.append(Y_train_temp)                 
-    	breakpoint            
-    	count = int(len(X_test)/10)
+    	sil = []
+        #dissimilarity would not be defined for a single cluster, thus, minimum number of clusters should be 2
+    	K = range(2,20);
+    	for k in K:
+            kmeans = KMeans(n_clusters = k).fit(X_train)
+            labels = kmeans.labels_
+            sil.append(silhouette_score(X_train, labels, metric = 'euclidean'))
+    	optimum_K = K[sil.index(max(sil))]
+    	plt.plot(K, sil, 'bx-')
+    	plt.xlabel('k')
+    	plt.ylabel('Silhouette Score')
+    	plt.title('Silhouette Dissimilarity Scores for various k')
+    	plt.axvline(x=optimum_K, color='r', linestyle='--')
+    	fig1 = plt.gcf()
+    	plt.show()
+    	plt.draw()
+    	fig1.savefig(dataset+'.png', format='png', bbox_inches='tight',dpi=300)
+    	plt.close()
+    	kmeans = KMeans(n_clusters = optimum_K, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)			
+    	kmeans.fit(X_train)
+    	count = len(np.unique(Y_train))
+    	for j in range(optimum_K):
+            X_train_temp = X_train[kmeans.labels_==j]
+            Y_train_temp = Y_train[kmeans.labels_==j]
+            v = len(np.unique(Y_train_temp))
+            if v > 1:
+                X_train_clusters.append(X_train_temp)
+                Y_train_clusters.append(Y_train_temp)  
+    	count = int(len(X_test)/5)
     	valX = X_test[0:count]
     	valy = Y_test[0:count]
     	X_test = X_test[count:]
