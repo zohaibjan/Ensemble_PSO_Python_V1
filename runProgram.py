a948b74c5e207ba6a365bbae00666d1d779e0792
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2020-02-04 16:10:52 +1000

    Update runProgram.py

--- runProgram.py
+++ runProgram.py
@@ -8,22 +8,29 @@ Created on Wed Dec  4 12:12:00 2019
 from mainProgram import mainProgram
 import numpy as np
 import csv
-
+import os.path
 
 data = {'thyroid','wine','diabetes',\
         'segment', 'ecoli','cancer',\
         'vehicle','iris','liver','ionosphere',\
         'sonar','glass'}
-numOfRuns = 10
-file = open("results.csv", mode="w")
+
+numOfRuns = 1
 for dataset in data:
+    if os.path.isfile('results.csv'):
+        file = open("results.csv", mode="a")
+        results = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)    
+    else:
+        file = open("results.csv", mode="w")
+        results = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)    
+        results.writerow(['dataset', 'accuracy','optimized_accuracy'])
     accuracy = np.zeros(numOfRuns)
     optimized_accuracy = np.zeros(numOfRuns)
+    results.writerow(['dataset', 'accuracy','optimized_accuracy'])
     for i in range(numOfRuns):
         temp = mainProgram(dataset)
         accuracy[i] = temp[0]
         optimized_accuracy = temp[1]
-    results = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     results.writerow([dataset, str(np.mean(accuracy)),str(np.mean(optimized_accuracy))])
 file.close();
 print ("Non_optimized and Optimized Accuracy for " + dataset  + " is: " + str(np.mean(accuracy)) + " and " + str(np.mean(optimized_accuracy)))
