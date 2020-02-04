9f0fd739aae05534c39783c2ddfc4d0230453df3
Muhammad Zohaib Jan <zohaibjan@gmail.com>
2020-02-04 16:11:47 +1000

    Update decisionFusion.py

--- decisionFusion.py
+++ decisionFusion.py
@@ -8,15 +8,10 @@ import numpy as np
 import pandas as pd
 
 def decisionFusion (ensemble, X, y):
-    acc = np.zeros(len(X))
     predictions = pd.DataFrame()
     for i in range(len(ensemble)):    
         predictions.insert(i,column = i, value = ensemble[i].predict(X))
     results = predictions.mode(axis = 'columns')[0]
     results = np.asarray(results)        
-    for i in range(len(results)):
-        if (int(results[i]) == y[i]):
-            acc[i] = 1
-        else:
-            acc[i] = 0
-    return np.mean(acc)
+    P = (results == y).mean()
+    return P
